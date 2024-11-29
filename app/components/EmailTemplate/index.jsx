9e360fb17ac6c34cdb8617292502609a3453270a
Aniket Singh <61254165+aniketsingh16@users.noreply.github.com>
2024-11-30 01:41:02 +0530

    Update index.jsx

--- app/components/EmailTemplate/index.jsx
+++ app/components/EmailTemplate/index.jsx
@@ -2,7 +2,7 @@
 export const EmailTemplate = ({ firstName }) => (
     <div>
       <h1>Welcome, {firstName}!</h1>
-      <p>Thank you for joining LoanDekho. We're excited to have you on board.</p>
+      <p>Thank you for joining LoanDekho. We&apos;re excited to have you on board.</p>
     </div>
   );
   
